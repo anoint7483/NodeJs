2bb6c0f0905d65ee0be07986e02cb6998a7afbaf
anoint7483 <yabhishek8774@gmail.com>
2025-02-14 23:44:21 +0530

    lecture 6 and 7 completed

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+// express app
+const app = express();
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// listen for requests 
+app.listen(3000);
+
+app.get('/', (req, res)=>{
+    // res.send('<p> home page </p>');
+    res.render('index', {title: 'Home'});
+});
+
+app.get('/about', (req, res)=>{
+    // res.send('<p> about page </p>');
+    res.render('about', {title: 'About'});
+});
+
+app.get('/blogs/create', (req, res)=>{
+    res.render('/create', {title: 'Create a new Blog'});
+});
+
+// 404 page
+app.use((req, res)=>{
+    res.status(404).render('404', {title: '404'});
+})
